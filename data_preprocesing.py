553bbc0db93b28b586e9734be458bde4e08742b6
MarkoKillin <mirkovicmarko790@gmail.com>
2024-06-11 17:51:24 +0200

    Implemented data_preprocesing.py and model.py

---
+++ data_preprocesing.py
@@ -0,0 +1,43 @@
+from torch import save
+from sklearn.datasets import fetch_lfw_people
+from torchvision import transforms
+from torch.utils.data import DataLoader, DataSet
+import numpy as np
+
+
+class LFWDataset(Dataset):
+    def __init__(self, images, labels, transform=None):
+        self.images = images
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image = self.images[idx]
+        label = self.labels[idx]
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+
+
+def prepare_date():
+    lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
+    X = lfw_people.images[:, :, :, np.newaxis] / 255.0
+    y = lfw_people.target
+
+    transform = transforms.Compose([
+        transforms.toPILImage(),
+        transforms.Resize((128, 128)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+
+    dataset = LFWDataset(X, y, transform=transform)
+    return dataset, lfw_people.target_names
+
+
+if __name__ == '__main__':
+    dataset, target_names = prepare_date()
+    print('----data loaded----')
---
+++ model.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class FaceRecognitionCNN(nn.Module):
+    def __init__(self, num_of_classes):  # tweak numbers
+        super(FaceRecognitionCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(128 * 16 * 16, 512)
+        self.fc2 = nn.Linear(512, num_of_classes)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.relu(F.max_pool2d(self.conv3(x), 2))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+def get_model(num_classes):
+    model = FaceRecognitionCNN(num_of_classes)
