6ef5ebc4d09b9a8986f86407c8f54b367f369a0c
MarkoKillin <mirkovicmarko790@gmail.com>
2024-06-18 17:15:47 +0200

    Augmenting resnet

--- model_resnet.py
+++ model_resnet.py
@@ -8,10 +8,26 @@ class FaceRecognitionResNet(nn.Module):
         self.resnet = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
         self.resnet.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
         nn.init.kaiming_normal_(self.resnet.conv1.weight, mode='fan_out', nonlinearity='relu')
+
+        self.dropout = nn.Dropout2d(p=0.5)
+
         self.resnet.fc = nn.Linear(self.resnet.fc.in_features, num_classes)
 
     def forward(self, x):
-        x = self.resnet(x)
+        x = self.resnet.conv1(x)
+        x = self.resnet.bn1(x)
+        x = self.resnet.relu(x)
+        x = self.resnet.maxpool(x)
+
+        x = self.resnet.layer1(x)
+        x = self.resnet.layer2(x)
+        x = self.resnet.layer3(x)
+        x = self.resnet.layer4(x)
+
+        x = self.resnet.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.dropout(x)
+        x = self.resnet.fc(x)
         return x
 
 
